a8c0a5ebe7cd0fcb72cb8e025568fbb32f812d6c
qoo <qoo@tt.com>
2026-03-20 17:07:02 +0800

    adding cloudflare url

--- jsscript/demo.js
+++ jsscript/demo.js
@@ -1,18 +1,18 @@
-const random_url = "";
-const API_URL = "https://" + random_url + "trycloudflare.com";
+const random_url = "talented-atm-published-prospect";
+const API_URL = "https://" + random_url + ".trycloudflare.com";
 async function connectToApi() {
   try {
     console.log("my test log")
-    // const response = await fetch(`{API_URL}`, {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // });
-    //
-    // const data = await response.json();
-    // console.log("from API get datas:", data);
-    // document.getElementById("apple").innerText = JSON.stringify(data);
+    const response = await fetch(API_URL + "/test01", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const data = await response.json();
+    console.log("from API get datas:", data);
+    document.getElementById("apple").innerText = JSON.stringify(data);
 
   } catch (error) {
     console.error("connection fail", error)
